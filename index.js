68f350be383677bfdc9f3e90847a0fdb29e13855
akshaykhatri-art <akshaykhatri22@gmail.com>
2023-02-24 14:00:55 +0530

    Products index route added and seeded data parsed into route

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ db.once("open", () => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products })
+})
+
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000!")
 })
